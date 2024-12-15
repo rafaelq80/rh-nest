5fce6d3839a09e3a12266d6e6a6d092787b11285
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-12-15 17:03:37 -0300

    :rocket: Configurações para Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ColaboradorModule } from './colaborador/colaborador.module';
-import { Colaborador } from './colaborador/entities/colaborador.entity';
+import { ProdService } from './data/services/prod.service';
 import { DepartamentoModule } from './departamento/departamento.module';
-import { Departamento } from './departamento/entities/departamento.entity';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_rh',
-      entities: [Departamento, Colaborador, Usuario],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     DepartamentoModule,
     ColaboradorModule,
---
+++ src/data/services/dev.service.ts
@@ -0,0 +1,22 @@
+﻿import { Injectable } from "@nestjs/common";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+import { Colaborador } from "../../colaborador/entities/colaborador.entity";
+import { Departamento } from "../../departamento/entities/departamento.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'db_rh',
+            entities: [Departamento, Colaborador, Usuario],
+            synchronize: true,
+    };
+  }
+}
